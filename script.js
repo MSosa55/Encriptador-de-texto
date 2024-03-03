85bcd5cc9c7146f3d3bc90059e0664af29b10382
Mariano Sosa <marianososallm@gmail.com>
2024-03-03 16:01:53 -0300

    Agregando funciones de encriptado y desencriptado

--- script.js
+++ script.js
@@ -8,15 +8,54 @@ const mensaje = document.querySelector(".cajaTexto2");
 //La letra "u" es convertida para "ufat"
 
 
+
+function botonEncriptar(){
+    const textoEncriptado = encriptar(textArea.value);
+    mensaje.value = textoEncriptado;
+    textArea.value = "";
+}
+
 function encriptar(cadenaEncriptada){
-    const matrizCodigo = [
+    let matrizCodigo = [
         ["e", "enter"],
         ["i", "imes"],
         ["a", "ai"],
         ["o", "ober"],
         ["u", "ufat"]
        ];
-       cadenaEncriptada = cadenaEncriptada.tolowerCase();
+       cadenaEncriptada = cadenaEncriptada.toLowerCase();
+
+       for(let i = 0; i < matrizCodigo.length;  i++){
+           if(cadenaEncriptada.includes(matrizCodigo[i][0])){
+            cadenaEncriptada = cadenaEncriptada.replaceAll(matrizCodigo[i][0], matrizCodigo[i][1]);
+           }
+       }
+    return cadenaEncriptada;   
+}
+
+
+function botonDesencriptar(){
+  const textoEncriptado = desencriptar(textArea.value);
+  mensaje.value = textoEncriptado;
+  textArea.value = "";
+}
+
+function desencriptar(cadenaDesencriptada){
+    let matrizCodigo = [
+        ["e", "enter"],
+        ["i", "imes"],
+        ["a", "ai"],
+        ["o", "ober"],
+        ["u", "ufat"]
+       ];
+    cadenaDesencriptada = cadenaDesencriptada.toLowerCase();
+    
+    for(let i = 0; i <matrizCodigo.length; i++){
+        if(cadenaDesencriptada.includes(matrizCodigo[i][1])){
+            cadenaDesencriptada = cadenaDesencriptada.replaceAll(matrizCodigo[i][1], matrizCodigo[i][0]);
+        }
+    }
+   return cadenaDesencriptada; 
 }
 
 
